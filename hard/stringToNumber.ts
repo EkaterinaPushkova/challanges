02598b9795e29a3e711f391f477db4792c540135
Ekaterina Pushkova <ekaterina.pushkova@acmecryptocorp.io>
2023-02-27 17:49:02 +0300

    refactor: change structure

--- hard/stringToNumber.ts
+++
@@ -1,9 +0,0 @@
-// Convert a string literal to a number, which behaves like Number.parseInt.
-
-type StringToNumber<RowMix extends string> =
-  RowMix extends `${infer NumberRow extends number}` ? NumberRow : never;
-//infer return type of NumberRow
-
-type MyNumber = StringToNumber<"123">;
-
-const myNumber: MyNumber = 123;
