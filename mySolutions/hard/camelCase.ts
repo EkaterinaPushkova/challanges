1f53e3580a395fa0f6bb739a83046e72d6f73b7c
Ekaterina Pushkova <ekaterina.pushkova@acmecryptocorp.io>
2023-03-01 15:17:58 +0300

    refactor: rename type

--- mySolutions/hard/camelCase.ts
+++ mySolutions/hard/camelCase.ts
@@ -5,9 +5,9 @@
 // type camelCase1 = CamelCase<'hello_world_with_types'> // expected to be 'helloWorldWithTypes'
 // type camelCase2 = CamelCase<'HELLO_WORLD_WITH_TYPES'> // expected to be same as previous one
 
-type RemoveUnderscore<Str> = Str extends `${infer A}_${infer Rest}`
+type CamelCase<Str> = Str extends `${infer A}_${infer Rest}`
   ? Uppercase<Str> extends Str
-    ? RemoveUnderscore<Lowercase<Str>>
-    : `${A}${Capitalize<RemoveUnderscore<Rest>>}`
+    ? CamelCase<Lowercase<Str>>
+    : `${A}${Capitalize<CamelCase<Rest>>}`
   : Str;
-type camelCase1 = RemoveUnderscore<"HELLO_WORLD_WITH_TYPES">;
+type camelCase1 = CamelCase<"HELLO_WORLD_WITH_TYPES">;
