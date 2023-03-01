3f8aec8348447dd26ebcfa662e8118c86366af50
Ekaterina Pushkova <ekaterina.pushkova@acmecryptocorp.io>
2023-03-01 15:08:54 +0300

    feat: add type camelcase for 1 test

---
+++ mySolutions/hard/camelCase.ts
@@ -0,0 +1,11 @@
+// Implement CamelCase<T> which converts snake_case string to camelCase.
+
+// For example
+
+// type camelCase1 = CamelCase<'hello_world_with_types'> // expected to be 'helloWorldWithTypes'
+// type camelCase2 = CamelCase<'HELLO_WORLD_WITH_TYPES'> // expected to be same as previous one
+
+type RemoveUnderscore<Str> = Str extends `${infer A}_${infer Rest}`
+  ? `${A}${Capitalize<RemoveUnderscore<Rest>>}`
+  : Str;
+type camelCase1 = RemoveUnderscore<"hello_world_with_types">;
